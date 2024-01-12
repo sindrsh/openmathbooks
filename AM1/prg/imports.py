d1b975ba1c2a072eb48d05c6f96c5032db740db5
sindre <sindre.heggen@gmail.com>
2024-01-12 22:50:53 +0100

    prgrm

---
+++ AM1/prg/imports.py
@@ -0,0 +1,10 @@
+# importerer konstanten pi fra modulen 'math'
+from math import pi
+print(pi)
+
+# importerer funksjonen randint fra modulen 'random'
+from random import randint
+
+# randint(2, 5) returnerer tilfeldig valgt heltall
+# mellom (og inkludert) 2 og 5.
+print(randint(2, 5))
