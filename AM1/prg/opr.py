be3c51ef596492c5eb26935744ff06210a7983b8
Sindre Sogge Heggen <sindre.heggen@gmail.com>
2025-05-30 10:15:05 +0200

    eks

--- AM1/prg/opr.py
+++ AM1/prg/opr.py
@@ -1,10 +1,10 @@
 a = 5 
 b = 2
 
-print("a+b = ", a+b);
-print("a-b = ", a-b);
-print ("a*b = " ,a*b);
-print ("a/b = " ,a/b);
-print("a**b = " ,a**b); # potens med grunntall a og 									      eksponent b
-print ("a//b = ", a//b); # a/b rundet ned til nærmeste 							        heltall
-print ("a%b = ", a%b); # resten til a//b
+print("a+b = ", a+b)
+print("a-b = ", a-b)
+print ("a*b = " ,a*b)
+print ("a/b = " ,a/b)
+print("a**b = " ,a**b) # potens med grunntall a og eksponent b
+print ("a//b = ", a//b) # a/b rundet ned til nærmeste heltall
+print ("a%b = ", a%b) # resten til a/b
